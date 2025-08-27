4c1bdb73baf4df5bc2086dcf1ec95478bc185934
tushar nair <tusharnair02@gmail.com>
2025-08-27 16:06:08 +0530

    one eternity later

--- flames.py
+++ flames.py
@@ -1,27 +1,31 @@
+def getRes(total_steps): 
+    flames = ['F','L','A','M','E','S'] 
+    start = 0
+    while len(flames) > 1:
+        indextopop =  (start + total_steps)%len(flames) - 1
+        start = indextopop%len(flames)
+        flames.pop(indextopop)
+    
+    return flames
 
 def flames(n1, n2):
-    res = ""
-    
     if len(n1) > len(n2):
         n1, n2 = n2, n1
 
     stack1 = list(n1)
     stack2 = list(n2)
+    stack3,stack4 = stack1[:],stack2[:]
 
-    print(stack1)
-    print(stack2)
-
-    for i in stack1:
-        if i in stack2:
+    for i in stack3:
+        if i in stack4:
             stack1.remove(i)
             stack2.remove(i)
 
-    print(stack1)
-    print(stack2)
-
     total  = int(len(stack1) + len(stack2))
 
-    print(total)
+    print(f'{total}\n')
+
+    res = getRes(total)
 
     return res
 
